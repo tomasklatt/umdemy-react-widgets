199cf19b44594bb84b1bb9f6d57b1567c877f325
Tomas Klatt <tomas.klatt@livesport.eu>
2022-04-19 08:37:17 +0200

    initial commit

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,7 +3,7 @@ import React from  'react';
 export default ( {items} ) => {
     const renderedItems = items.map( item => {
         return (
-            <div key={item.title}>
+            <React.Fragment key={item.title}>
                 <div className="title active">
                     <i className="dropdown icon"></i>
                     {item.title}
@@ -13,7 +13,7 @@ export default ( {items} ) => {
                         {item.content}
                     </p>
                 </div>
-            </div>
+            </React.Fragment>
         );
     });
 
