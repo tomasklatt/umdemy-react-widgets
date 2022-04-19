556919cdfbb3958c56ae965ac453b32438b78e1e
Tomas Klatt <tomas.klatt@livesport.eu>
2022-04-19 15:43:57 +0200

    initial commit

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,10 +2,23 @@ import React from "react";
 
 const Dropdown =  ( {options}) => {
     const values = options.map(( {label, value}) => {
-        return <option value={value} key={value}>{label}</option>
+        return <div key={value} className="item">{label}</div>
     });
     return (
-        <select id="Dropdown">{values}</select>
+        <div className="ui form">
+            <div className="field">
+                <label className="label">Select a color</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">
+                        Select Color
+                    </div>
+                    <div className="menu visible transition">
+                        {values}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
