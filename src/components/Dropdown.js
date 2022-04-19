08751956decf9dad4ddc1b45ba88fefbbbb8d66e
Tomas Klatt <tomas.klatt@livesport.eu>
2022-04-19 16:06:47 +0200

    initial commit

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Dropdown =  ( {options, selected, onSelectedChanged}) => {
+    const [active, setActive] = useState(false);
+
     const values = options.map(( option) => {
+        if(option.value === selected.value){
+            return null;
+        }
         return <div key={option.value} className="item" onClick={() => {onSelectedChanged(option)}}>{option.label}</div>
     });
     return (
         <div className="ui form">
             <div className="field">
                 <label className="label">Select a color</label>
-                <div className="ui selection dropdown visible active">
+                <div
+                    onClick={() => setActive(!active)}
+                    className={`ui selection dropdown${active ? ' visible active' : ''}`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">
                         {selected.label}
                     </div>
-                    <div className="menu visible transition">
+                    <div className={`menu transition${active ? ' visible' : ''}`}>
                         {values}
                     </div>
                 </div>
