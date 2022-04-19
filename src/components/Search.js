f865d1aefc481c8f8b7ca5302b28d3cab468d164
Tomas Klatt <tomas.klatt@livesport.eu>
2022-04-19 11:51:34 +0200

    initial commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import axios, {Axios} from "axios";
+import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState('');
