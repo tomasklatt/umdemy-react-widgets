92bacd46e528834cfb6b976e8eb09adebd87d40c
Tomas Klatt <tomas.klatt@livesport.eu>
2022-04-19 08:27:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Accordion from "./components/Accordion";
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Accordion/>
+        </div>
     );
 }
 
