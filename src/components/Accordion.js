2c80f65b0064eaeda38062c1a1be4023f7b6b30c
Tomas Klatt <tomas.klatt@livesport.eu>
2022-04-19 09:15:31 +0200

    initial commit

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,16 @@
-import React from  'react';
+import React, {useState} from  'react';
 
-export default ( {items} ) => {
-    const renderedItems = items.map( item => {
+const Accordion =  ( {items} ) => {
+    const [activeIndex, setActiveIndex] = useState(null)
+
+    const onTitleClicked = (index) => {
+        setActiveIndex(index)
+    }
+
+    const renderedItems = items.map( (item, index) => {
         return (
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div className="title active" onClick={() => onTitleClicked(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
@@ -20,6 +26,9 @@ export default ( {items} ) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
+            <h1>{activeIndex}</h1>
         </div>
     );
 }
+
+export default Accordion;
