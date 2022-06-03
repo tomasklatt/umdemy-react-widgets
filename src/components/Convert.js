209522eda408073683ffc3207745ee05a4210c03
Tomas Klatt <tomas.klatt@livesport.eu>
2022-06-03 12:55:58 +0200

    initial commit

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,16 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const Convert = ({ languageCode, text}) => {
     useEffect(() => {
         console.log(`${languageCode} - ${text}`)
+        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+            params: {
+                q: text,
+                target: languageCode,
+                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+            }
+        })
     }, [languageCode, text]);
 
     return (
