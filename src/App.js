e1531f8cb85b773a86340cc37a8b7973746ab86e
Tomas Klatt <tomas.klatt@livesport.eu>
2022-06-03 14:52:20 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
     {
@@ -34,12 +35,47 @@ const colors = [
     },
 ];
 
+const showAccordion = () => {
+    if (window.location.pathname === '/'){
+        return <Accordion items={items} />
+    }
+}
+
+const showList = () => {
+    if (window.location.pathname === '/list'){
+        return <Search items={items} />
+    }
+}
+
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown'){
+        return <Dropdown  />
+    }
+}
+
+const showTranslate = () => {
+    if (window.location.pathname === '/translate'){
+        return <Translate />
+    }
+}
+
 const App = () => {
     const [selected, setSelected] = useState(colors[0]);
 
     return (
         <div>
-            <Translate/>
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search items={items} />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown objectOfSelecting="color" selected={selected} onSelectedChanged={setSelected} options={colors}/>
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 }
